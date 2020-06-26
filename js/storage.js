2f65ed3806060d47c2b32c2823d597c434e4e88f
Luoyu <1197182168@qq.com>
2020-06-26 21:19:17 +0800

    Extract storage module

--- js/storage.js
+++ js/storage.js
@@ -1,35 +1,72 @@
-(function () {
-    'use strict';
+'use strict';
 
-    const db = 'mytodo';
+const DB = 'mytodo';
 
-    function add(item){
-        var items = JSON.parse(localStorage.getItem(db));
-        items.push(item);
-        localStorage.setItem(db, JSON.stringify(items));
+function initDB() {
+    if (!localStorage.getItem(DB)) {
+        localStorage.setItem(DB, '[]');
     }
+}
 
-    function remove(id){
-        var items = JSON.parse(localStorage.getItem(db));
-        for(var i = 0; i < items.length; ++i){
-            if(items[i].id == id){
-                items.splice(i, 1);
-                return;
-            }
+function add(item) {
+    var items = JSON.parse(localStorage.getItem(DB));
+    items.push(item);
+    localStorage.setItem(DB, JSON.stringify(items));
+}
+
+function remove(id) {
+    var items = JSON.parse(localStorage.getItem(DB));
+    for (let i = 0; i < items.length; ++i) {
+        if (items[i].id == id) {
+            items.splice(i, 1);
+            break;
         }
-        localStorage.setItem(db, JSON.stringify(items));
     }
+    localStorage.setItem(DB, JSON.stringify(items));
+}
 
-    function update(id, item){
-        var items = JSON.parse(localStorage.getItem(db));
-        for(var i = 0; i < items.length; ++i){
-            if(items[i].id == id){
-                items[i].completed = item.completed;
-                items[i].date = item.date;
-                items[i].detail = item.detail;
-            }
+function get(id) {
+    var items = JSON.parse(localStorage.getItem(DB));
+    for (let i = 0; i < items.length; ++i) {
+        if (items[i].id == id) {
+            return items[i];
         }
-        localStorage.setItem(db, JSON.stringify(items));
     }
-    
-})();
+}
+
+function getAll() {
+    return JSON.parse(localStorage.getItem(DB));
+}
+
+function update(id, item) {
+    var items = JSON.parse(localStorage.getItem(DB));
+    for (let i = 0; i < items.length; ++i) {
+        if (items[i].id == id) {
+            items[i].completed = item.completed;
+            items[i].date = item.date;
+            items[i].detail = item.detail;
+            break;
+        }
+    }
+    localStorage.setItem(DB, JSON.stringify(items));
+}
+
+function updateAll(items) {
+    localStorage.setItem(DB, JSON.stringify(items));
+}
+
+function updateAllCompleted(completed) {
+    var items = JSON.parse(localStorage.getItem(DB));
+    for (let i = 0; i < items.length; ++i) {
+        items[i].completed = completed;
+    }
+    localStorage.setItem(DB, JSON.stringify(items));
+}
+
+function setStatus(status) {
+    localStorage.setItem('status', status);
+}
+
+function getStatus() {
+    return localStorage.getItem('status');
+}
