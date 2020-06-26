79f693cedb7777913bbf9fd04f9428a23b0d8c2d
Luoyu <1197182168@qq.com>
2020-06-27 00:57:40 +0800

    Add comments

--- js/mytodo.js
+++ js/mytodo.js
@@ -5,10 +5,17 @@
 
 'use strict';
 
-const SECONDS = 1000 * 60 * 60 * 24;
+// Milliseconds per day
+const PER_DAY = 1000 * 60 * 60 * 24;
+// Abbreviations of 12 months
 const MONTHS = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+// Milliseconds per second
 const PER_SECOND = 1000;
 
+
+/**
+ * @description: Set time in customized format and update it
+ */
 function updateTime() {
     var date = new Date();
     var year = date.getFullYear();
@@ -24,12 +31,16 @@ function updateTime() {
     document.getElementById('year').innerHTML = year;
 }
 
+/**
+ * @description: Initialize filter menu
+ */
 function initMenu() {
     var all = document.getElementById('all');
     var active = document.getElementById('active');
     var completed = document.getElementById('completed');
     var clearCompleted = document.getElementById('clear-completed');
 
+    // Add filter according to the status in localStorage
     if (getStatus()) {
         all.classList.remove('selected');
         active.classList.remove('selected');
@@ -37,6 +48,7 @@ function initMenu() {
         document.getElementById(getStatus()).classList.add('selected');
     }
 
+    // Add click listener for filter 'all'
     all.addEventListener('click', function () {
         all.classList.add('selected');
         active.classList.remove('selected');
@@ -45,6 +57,7 @@ function initMenu() {
         updatePage();
     });
 
+    // Add click listener for filter 'active'
     active.addEventListener('click', function () {
         all.classList.remove('selected');
         active.classList.add('selected');
@@ -53,6 +66,7 @@ function initMenu() {
         updatePage();
     });
 
+    // Add click listener for filter 'completed'
     completed.addEventListener('click', function () {
         all.classList.remove('selected');
         active.classList.remove('selected');
@@ -61,6 +75,7 @@ function initMenu() {
         updatePage();
     });
 
+    // Add click listener for clearing completed todos
     clearCompleted.addEventListener('click', function () {
         var container = document.getElementById('todo-list-container');
         var todoItems = document.getElementsByClassName('todo-item');
@@ -75,6 +90,7 @@ function initMenu() {
                 } else {
                     todoContent.removeChild(todoItems[i]);
                 }
+                // Ensure to get todoItems[i+1] in next loop
                 i--;
             }
         }
@@ -82,10 +98,16 @@ function initMenu() {
     });
 }
 
+
+/**
+ * @description: Initialize todo input
+ */
 function initInput() {
+    // Add click listener for toggling all todos
     var toggleAll = document.getElementById('toggle-all');
     toggleAll.addEventListener('click', function () {
         let todoItems = document.getElementsByClassName('todo-item');
+        // All completed to all incompleted, part completed to all completed
         if (toggleAll.classList.contains('all-checked')) {
             updateAllCompleted(false);
             toggleAll.classList.remove('all-checked');
@@ -110,6 +132,7 @@ function initInput() {
         updatePage();
     });
 
+    // Add 'Enter' keyup listener for todo input
     var newDate = document.getElementById('new-date');
     newDate.valueAsDate = new Date();
     var newTodo = document.getElementById('new-todo');
@@ -126,6 +149,9 @@ function initInput() {
     });
 }
 
+/**
+ * @description: Initialize todolists
+ */
 function initTodoLists() {
     var todoItems = getAll();
     for (let i = 0; i < todoItems.length; ++i) {
@@ -133,17 +159,29 @@ function initTodoLists() {
     }
 }
 
+/**
+ * @description: Add a todo according to inputs
+ * @param {String} detail
+ * @param {Date} date
+ */
 function addTodoItem(detail, date) {
     var item = {};
     item.id = new Date().getTime();
     item.completed = false;
     item.date = date;
     item.detail = detail;
-    add(item);
 
+    // Add an item in localStorage
+    add(item);
     addTodoItemUI(date, item)
 }
 
+
+/**
+ * @description: Add a todo item if date exits or a todo list if date doesn't exit
+ * @param {Date} date
+ * @param {item} - {"id": new Date.getTime(), "completed": true/false, "date": "xxxx-xx-xx", "detail": }
+ */
 function addTodoItemUI(date, item) {
     var todoDates = document.getElementsByClassName('todo-date');
     if (todoDates.length == 0) {
@@ -151,6 +189,7 @@ function addTodoItemUI(date, item) {
     } else {
         for (let i = 0; i < todoDates.length; i++) {
             if (date == todoDates[i].innerHTML) {
+                // Date of item already exits
                 addTodoItemInDate(todoDates[i].nextElementSibling, item);
                 initTodoItem(item.id, item);
                 updatePage();
@@ -160,10 +199,15 @@ function addTodoItemUI(date, item) {
         addTodoList(item);
     }
 
+    // Date of item doesn't exit
     initTodoItem(item.id, item);
     updatePage();
 }
 
+/**
+ * @description: Add a todo list
+ * @param {item} item
+ */
 function addTodoList(item) {
     var container = document.getElementById('todo-list-container');
     var todoList = document.createElement('div');
@@ -171,13 +215,16 @@ function addTodoList(item) {
     var status;
     var now = new Date().getTime();
     var date = new Date(item.date).getTime();
-    if ((date - now) / SECONDS < -1) {
+
+    // Set todo status according to deadline
+    if ((date - now) / PER_DAY < -1) {
         status = 'expired';
-    } else if ((date - now) / SECONDS < 3) {
+    } else if ((date - now) / PER_DAY < 3) {
         status = 'urgent';
     } else {
         status = 'adequate';
     }
+    
     todoList.innerHTML = [
         '<div class="todo-date ' + status + '">' + item.date + '</div>',
         '<ul class="todo-content">',
@@ -194,6 +241,7 @@ function addTodoList(item) {
         container.appendChild(todoList);
     } else {
         for (let i = 0; i < todoDates.length; i++) {
+            // Todo lists sorted by date ascending
             let curDate = new Date(todoDates[i].innerText);
             if (date < curDate) {
                 container.insertBefore(todoList, todoDates[i].parentNode);
@@ -204,6 +252,11 @@ function addTodoList(item) {
     }
 }
 
+/**
+ * @description: Add a todo item in param todoContent
+ * @param {<ul>} todoContent
+ * @param {item} item
+ */
 function addTodoItemInDate(todoContent, item) {
     var todoItem = document.createElement('li');
     todoItem.id = item.id;
@@ -216,6 +269,11 @@ function addTodoItemInDate(todoContent, item) {
     todoContent.appendChild(todoItem);
 }
 
+/**
+ * @description: Get offsetLeft, offsetTop, offsetWidth, offsetHeight of item
+ * @param {<div todoitem>} item 
+ * @return: An array consists of four offsets
+ */
 function getOffset(item) {
     var offset = [];
     offset[0] = item.offsetLeft;
@@ -225,11 +283,19 @@ function getOffset(item) {
     return offset;
 }
 
+
+/**
+ * @description: Initialize all todo items
+ * @param {Number} id
+ * @param {<div todo-item>} item
+ */
 function initTodoItem(id, item) {
     var todoItem = document.getElementById(id);
     var drag = false;
+    // Blank alternative element while dragging
     var dragAlt;
 
+    // Add touch start listener for todo item
     todoItem.addEventListener('touchstart', function (event) {
         drag = true;
         todoItem.classList.add('dragging');
@@ -238,6 +304,8 @@ function initTodoItem(id, item) {
         todoItem.x = event.touches[0].clientX - offset[0];
         todoItem.y = event.touches[0].clientY - offset[1];
 
+        // Set the style.position of dragged item as absolute
+        // Easier to set absolute position of dragged item while dragging
         todoItem.style.position = 'absolute';
         todoItem.style.left = offset[0] + 'px';
         todoItem.style.top = offset[1] + 'px';
@@ -250,27 +318,35 @@ function initTodoItem(id, item) {
         todoItem.parentElement.insertBefore(dragAlt, todoItem);
     }, false);
 
+    // Add touch move listener for todo item
     todoItem.addEventListener('touchmove', function (event) {
         if (drag) {
+            // Set absolute position of dragged item while touch moves
             todoItem.style.left = (event.touches[0].clientX - todoItem.x) + 'px';
             todoItem.style.top = (event.touches[0].clientY - todoItem.y) + 'px';
-
+            
+            // Check all todo items and find one to change its position
             let todoItems = document.getElementsByClassName('todo-item');
             for (let i = 0; i < todoItems.length; ++i) {
                 if (todoItems[i].id == todoItem.id) {
                     continue;
                 }
                 let offset = getOffset(todoItems[i]);
+                // Check if the touch point are in the area of todoItems[i]
                 if ((event.touches[0].clientX > offset[0]) && (event.touches[0].clientX < offset[0] + offset[2]) &&
                     (event.touches[0].clientY > offset[1]) && (event.touches[0].clientY < offset[1] + offset[3])) {
+                    // Check if clientY of touch point is above a half of todoItems[i] while moving up
                     if (event.touches[0].clientY < offset[1] + offset[3] / 2) {
+                        // If it is. insert drag alternative before todoItems[i]
                         todoItems[i].parentElement.insertBefore(dragAlt, todoItems[i]);
                         break;
                     } else {
+                        // If todoItems[i] is the last child in one todo list, use appendChild() instead of insertBefore
                         if (!todoItems[i].nextElementSibling) {
                             todoItems[i].parentElement.appendChild(dragAlt);
                             break;
                         } else {
+                            // clientY of touch point is below a half of todoItems[i] while moving down
                             todoItems[i].parentElement.insertBefore(dragAlt, todoItems[i].nextElementSibling);
                             break;
                         }
@@ -282,6 +358,7 @@ function initTodoItem(id, item) {
         }
     }, false);
 
+    // Add touch end listener for todo item
     todoItem.addEventListener('touchend', function (event) {
         if (drag) {
             todoItem.style.position = 'static';
@@ -291,7 +368,8 @@ function initTodoItem(id, item) {
             drag = false;
 
             let offset = getOffset(todoItem);
-            if (event.changedTouches[0].clientX > offset[0] + 0.85 * offset[2]) {
+            // If clientX of touch end point is in the 20% of todo item in th rightside, then reove this item
+            if (event.changedTouches[0].clientX > offset[0] + 0.8 * offset[2]) {
                 let todoContent = todoItem.parentElement;
                 let todoList = todoContent.parentElement;
                 let container = document.getElementById('todo-list-container');
@@ -302,6 +380,7 @@ function initTodoItem(id, item) {
                 }
             }
 
+            // If touch end point is in the area of todo date tile, then add the dragged item to the end of the corresponding list
             let todoContents = document.getElementsByClassName('todo-content');
             for (let i = 0; i < todoContents.length; ++i) {
                 let offset = getOffset(todoContents[i].parentElement.querySelector('.todo-date'));
@@ -311,16 +390,19 @@ function initTodoItem(id, item) {
                 }
             }
 
+            // Get all items from page and update localStorage
             updateAllFromPage();
+            // Remove todo lists without any todo item
             updatePage();
         }
     }, false);
 
-
+    // Add click listener for toggle button
     todoItem.querySelector('.toggle').addEventListener('click', function () {
         updateTodo(id, item);
     });
 
+    // Add dblclick listener for todo detail
     var todoDetail = todoItem.querySelector('.todo-detail');
     todoDetail.addEventListener('dblclick', function () {
         todoItem.classList.add('editing');
@@ -356,11 +438,15 @@ function initTodoItem(id, item) {
         edit.focus();
     }, false);
 
+    // Add click listener for delete button
     todoItem.querySelector('.delete').addEventListener('click', function () {
         removeTodo(id);
     });
 }
 
+/**
+ * @description: Get all items from page and update localStorage
+ */
 function updateAllFromPage() {
     var todoItems = document.getElementsByClassName('todo-item');
     var items = [];
@@ -375,6 +461,11 @@ function updateAllFromPage() {
     updateAll(items);
 }
 
+/**
+ * @description: Update the attribute completed for item with id and change css of it
+ * @param {Number} id
+ * @param {item} item
+ */
 function updateTodo(id, item) {
     item.completed = !item.completed;
     update(id, item);
@@ -391,13 +482,19 @@ function updateTodo(id, item) {
     updatePage();
 }
 
+/**
+ * @description: Remove todo item
+ * @param {Number} id
+ */
 function removeTodo(id) {
     remove(id);
     var container = document.getElementById('todo-list-container');
     var todoItem = document.getElementById(id);
     var todoContent = todoItem.parentElement;
     var todoList = todoContent.parentElement;
+    // If the todo list only has one todo item and this item is going to be removed 
     if (todoContent.querySelectorAll('.todo-item').length == 1) {
+        // Remove the whole list from container
         container.removeChild(todoList);
     } else {
         todoContent.removeChild(todoItem);
@@ -405,6 +502,9 @@ function removeTodo(id) {
     updatePage();
 }
 
+/**
+ * @description: Update the whole page including filter menu, todo lists, toggle status and so on
+ */
 function updatePage() {
     var newTodo = document.getElementById('new-todo');
     var container = document.getElementById('todo-list-container');
@@ -418,6 +518,7 @@ function updatePage() {
         if (!todoDetail.classList.contains('completed')) {
             leftCount++;
         }
+        // Filter todo lists combining menu filter with text filter
         if ((menuFilter == 'All' || (menuFilter == 'Active' && !todoDetail.classList.contains('completed')) ||
                 (menuFilter == 'Completed' && todoDetail.classList.contains('completed'))) && (todoDetail.innerHTML.toUpperCase().indexOf(textFilter) > -1)) {
             if (todoItems[i].parentElement.parentElement.style.display == 'none') {
@@ -440,6 +541,7 @@ function updatePage() {
         }
     }
 
+    // If a todo list doesn't have any todo item, then remove it 
     let todoContents = document.getElementsByClassName('todo-content');
     for (let i = 0; i < todoContents.length; ++i) {
         if (todoContents[i].children.length == 0) {
@@ -448,6 +550,7 @@ function updatePage() {
         }
     }
 
+    // Update left todo count
     var completedCount = todoItems.length - leftCount;
     var count = document.getElementById('count');
     count.innerHTML = (leftCount || '0') + ' todo(s) left';
